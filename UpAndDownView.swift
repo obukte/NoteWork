93fab1a3027c01ac94953bbc8cde1cbb4142e48e
obukte <omer.bukte@ozu.edu.tr>
2017-05-14 00:20:56 +0300

    Added all files

---
+++ UpAndDownView.swift
@@ -0,0 +1,25 @@
+//
+//  UpAndDownView.swift
+//  NoteWork
+//
+//  Created by OMER BUKTE on 5/4/17.
+//  Copyright © 2017 Omer Bukte. All rights reserved.
+//
+
+import UIKit
+
+class UpAndDownView: UIView {
+
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor.init(red: SHADOW_GREY , green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+        
+    }
+
+}
